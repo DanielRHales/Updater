087298a0c375f5cb80bd478c6f5ae44a0cff1ff8
Daniel <silverman1963@hotmail.com>
2016-08-30 18:53:21 -0700

    Name Change Finalization & Builds

--- src/main/java/com/config/Constants.java
+++ src/main/java/com/config/Constants.java
@@ -8,14 +8,14 @@ import java.io.File;
  */
 public class Constants {
 
-    private static final String SERVER_NAME = "NoxiousPs";
+    private static final String SERVER_NAME = "Noxious";
 
     public static final File MAIN_DIRECTORY = new File(System.getProperty("user.home"), SERVER_NAME);
     public static final File ERROR_DIRECTORY = new File(MAIN_DIRECTORY, ".UpdaterErrors");
     public static final File PROGRAM_DIRECTORY = new File(MAIN_DIRECTORY, ".Launcher");
     public static final File PROGRAM_FILE = new File(PROGRAM_DIRECTORY, "Launcher.jar");
 
-    private static final String REMOTE_ROOT_LINK = String.format("http://cache.%s.com/", SERVER_NAME);
+    private static final String REMOTE_ROOT_LINK = String.format("http://cache.%sPs.com/", SERVER_NAME);
     public static final String REMOTE_PROGRAM_LINK = String.format("%sLauncher.jar", REMOTE_ROOT_LINK);
 
     private static final String[] ALGORITHMS = new String[]{"MD5", "SHA-1", "SHA-256"};
